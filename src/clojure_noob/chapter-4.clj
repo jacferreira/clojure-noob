2fdc47021b315dd618d8e9f878d885b2568f2b73
João Ferreira <joao.ferreira@siscog.pt>
2015-05-28 15:51:27 +0100

    finished chapter 4. started project euler.

--- src/clojure_noob/chapter-4.clj
+++ src/clojure_noob/chapter-4.clj
@@ -26,3 +26,42 @@
   [part]
   {:name (clojure.string/replace (:name part) #"^left-" "right-")
    :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps which have a :name and :size"
+  [asym-body-parts] ;
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts
+            final-body-parts (conj final-body-parts part)]
+        (if (needs-matching-part? part)
+          (recur remaining (conj final-body-parts (make-matching-part part)))
+          (recur remaining final-body-parts))))))
+
+(symmetrize-body-parts asym-hobbit-body-parts)
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps which have a :name and :size."
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (let [final-body-parts (conj final-body-parts part)]
+              (if (needs-matching-part? part)
+                (conj final-body-parts (make-matching-part part))
+                final-body-parts)))
+          []
+          asym-body-parts))
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + 0 (map :size sym-parts))
+        target (inc (rand body-part-size-sum))]
+    (loop [[part & rest] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur rest (+ accumulated-size (:size part)))))))
+
+(hit asym-hobbit-body-parts)
---
+++ src/project_euler/problem_003.clj
@@ -0,0 +1,1 @@
+(ns project-euler.problem-003)

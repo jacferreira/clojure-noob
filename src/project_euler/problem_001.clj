565802dc1cb5a9a3ce89a0e6e4a798efdf53aa2d
João Ferreira <joao.ferreira@siscog.pt>
2015-06-02 14:41:27 +0100

    Removed project-euler files. Renamed files to reflect book chapters.

--- src/project_euler/problem_001.clj
+++
@@ -1,9 +0,0 @@
-(ns project-euler.problem-001)
-
-(defn sum-multi-3-and-5 [limit]
-  (reduce + (filter (fn [n]
-                      (or (zero? (rem n 3))
-                          (zero? (rem n 5))))
-                    (range 1 limit))))
-
-(sum-multi-3-and-5 1000)
--- src/project_euler/problem_002.clj
+++
@@ -1,9 +0,0 @@
-(ns project-euler.problem-002)
-
-(def fib-seq
-  ((fn fib [a b] (lazy-seq (cons a (fib b (+' a b))))) 1 1))
-
-(defn sum-even-fib [max]
-  (reduce + (filter even? (take-while #(< % max) fib-seq))))
-
-(sum-even-fib 4000000)
